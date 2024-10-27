4ed27d9049f2a8592fe67489eb28ac0ede9f0ea2
Aayan Ahmed <aayanahmedtejani@gmail.com>
2024-10-27 20:38:38 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,22 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      {product &&
-        product.map((e, i) => {
-          return <Card key={i} title={e.title} image={e.thumbnail} desc={e.description}/>;
-        })}
+    <div className="px-6 py-4 bg-gray-400">
+      {product && (
+        <div className="grid grid-cols-3 md:grid-cols-6 lg:grid-cols-12 sm:gap-6 md:gap-10  mx-auto gap-2">
+          {product.map((e, i) => {
+            return (
+              <Card
+                key={i}
+                title={e.title}
+                image={e.thumbnail}
+                desc={e.description}
+                price={Math.round(e.price * 100)}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-const Card = ({title, image, desc}) => {
+const Card = ({ title, image, desc, price }) => {
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
-      <figure>
-        <img
-          src={image}
-          alt={title}
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{title}</h2>
-        <p>{desc}</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+    <div className="mx-auto col-span-2 md:col-span-3 lg:col-span-4">
+      <div className="card bg-base-100 col-span-1 md:col-span-2 lg:col-span-2 shadow-xl">
+        <figure className="border-b-2 border-slate-900">
+          <img src={image} alt={title} />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-2xl">Rs: {price}</h2>
+          <h2 className="card-title">{title}</h2>
+          <p className="text-lg leading-5">{desc}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Add To Cart</button>
+          </div>
         </div>
       </div>
     </div>
